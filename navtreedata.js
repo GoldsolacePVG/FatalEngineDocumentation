2de2476902f61f3fb613f9bd83dd99364923c2d7
GoldsolacePVG <rodrigomicopliego@gmail.com>
2025-03-12 20:07:58 +0100

    Second commit

--- navtreedata.js
+++ navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "FatalEngine", "index.html", [
+  [ "Fatal Engine", "index.html", [
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
       [ "Namespace Members", "namespacemembers.html", [
